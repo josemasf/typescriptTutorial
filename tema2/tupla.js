a024abff7401bfa35c27fb0f9c596b3425f3554c
josemasf <josemasf@gmail.com>
2020-05-06 20:54:29 +0200

    tuplas

---
+++ tema2/tupla.js
@@ -0,0 +1,8 @@
+var vector;
+var vector2;
+vector = [4, 5];
+vector2 = [23, 7];
+var vectorREsultado = [vector[0] + vector2[0], vector[1] + vector2[1]];
+console.log(vectorREsultado);
+var datos = ["Telefono", 123];
+console.log(datos);
---
+++ tema2/tupla.ts
@@ -0,0 +1,12 @@
+let vector: [number, number];
+let vector2: [number, number];
+
+vector = [4,5];
+vector2 = [23,7];
+
+let vectorREsultado: [number, number] = [vector[0]+ vector2[0], vector[1] + vector2[1]];
+
+console.log(vectorREsultado);
+
+let datos: [string, number] = ["Telefono", 123];
+console.log(datos);
