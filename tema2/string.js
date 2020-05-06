fa9395eade74a973157d1d9f2a1f4f3ba4ff63f4
josemasf <josemasf@gmail.com>
2020-05-06 20:32:07 +0200

    cadenas

---
+++ tema2/string.js
@@ -0,0 +1,10 @@
+var pelicula = "El señor de los anillos";
+var serie = 'Big bang theory';
+var poema = "Por cien ca\u00F1ones\na toda vela\nno sucar el mar\nsi no vuela\nun velor mercantil";
+//operaciones
+var programa = "Mis programas favoritos son " + pelicula + " y " + serie;
+console.log(programa);
+var programas = "mis pogramas\n\nson: " + pelicula + " y " + serie + "\n\n";
+//metodos
+console.log(programa.charAt(0));
+console.log(programa.toUpperCase());
---
+++ tema2/string.ts
@@ -0,0 +1,25 @@
+let pelicula: string ="El señor de los anillos";
+let serie: string = 'Big bang theory';
+let poema: string = `Por cien cañones
+a toda vela
+no sucar el mar
+si no vuela
+un velor mercantil`;
+
+//operaciones
+
+let programa: string = "Mis programas favoritos son " + pelicula + " y " +serie;
+console.log(programa);
+
+
+let programas: string = `mis pogramas
+
+son: ${pelicula} y ${serie}
+
+`
+
+
+//metodos
+
+console.log(programa.charAt(0));
+console.log(programa.toUpperCase());
