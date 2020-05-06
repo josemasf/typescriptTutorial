8892c477dc6b5999b17caceb8e54078f5217cd9a
josemasf <josemasf@gmail.com>
2020-05-06 21:04:31 +0200

    tipo dato void para funciones

---
+++ tema2/void.js
@@ -0,0 +1,9 @@
+var inutil = undefined;
+var vacio = null;
+var respuestaServer = "Datos cargados";
+if (respuestaServer != vacio) {
+    console.log("Carga página");
+}
+else {
+    console.log("Error");
+}
---
+++ tema2/void.ts
@@ -0,0 +1,10 @@
+let inutil: void = undefined;
+let vacio: void = null;
+
+let respuestaServer: any = "Datos cargados";
+
+if(respuestaServer!= vacio){
+    console.log("Carga página");
+}else{
+    console.log("Error");
+}
