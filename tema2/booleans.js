0961db66195ea49030cb495cb3e2a6a80fe678b2
josemasf <josemasf@gmail.com>
2020-05-06 20:06:11 +0200

    buleanos

---
+++ tema2/booleans.js
@@ -0,0 +1,9 @@
+var soyAlto = true;
+var soyFan = false;
+console.log(soyAlto, 'soy alto');
+console.log(soyFan, 'soy fan');
+//operación conjuncion
+console.log(soyAlto && soyFan);
+//operación disyuncion
+console.log(soyAlto || soyFan);
+console.log(soyAlto.valueOf);
---
+++ tema2/booleans.ts
@@ -0,0 +1,12 @@
+let soyAlto: boolean = true;
+let soyFan: boolean = false;
+
+console.log(soyAlto, 'soy alto');
+console.log(soyFan, 'soy fan');
+//operación conjuncion
+console.log(soyAlto && soyFan);
+//operación disyuncion
+console.log(soyAlto || soyFan);
+
+
+console.log(soyAlto.valueOf);
