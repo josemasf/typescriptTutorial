e86c106598b158427c224137730fd1d63cc994c8
josemasf <josemasf@gmail.com>
2020-05-06 21:00:16 +0200

    tipo any cualquiera

---
+++ tema2/any.js
@@ -0,0 +1,7 @@
+var obtjetoLibreria;
+obtjetoLibreria = 3;
+obtjetoLibreria = true;
+obtjetoLibreria = ['Hola', 8];
+//podría tener atributo
+console.log(obtjetoLibreria.atributoDesconocido);
+var lista = ["hola", true, 12.3];
---
+++ tema2/any.ts
@@ -0,0 +1,11 @@
+let obtjetoLibreria: any;
+
+obtjetoLibreria = 3;
+obtjetoLibreria = true;
+obtjetoLibreria = ['Hola', 8];
+
+//podría tener atributo
+console.log(obtjetoLibreria.atributoDesconocido);
+
+
+let lista: any[] = ["hola", true, 12.3];
