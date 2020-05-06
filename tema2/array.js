1fca640bb47f542da78a5848130abecc49a2ed3b
josemasf <josemasf@gmail.com>
2020-05-06 20:45:01 +0200

    arrays

---
+++ tema2/array.js
@@ -0,0 +1,7 @@
+var misNumeros = [1, 2, 3];
+var misNotas = [4, 5, 6];
+misNumeros.push(55);
+console.log(misNumeros[1]);
+var misJugadores = ["Casillas", "Bufon", "Manu Leal"];
+misJugadores.push("Cristian");
+console.log(misJugadores[2]);
---
+++ tema2/array.ts
@@ -0,0 +1,9 @@
+let misNumeros: number[] = [1,2,3];
+let misNotas: Array<number> = [4,5,6];
+
+misNumeros.push(55);
+console.log(misNumeros[1]);
+
+let misJugadores: Array<string> = ["Casillas", "Bufon", "Manu Leal"];
+misJugadores.push("Cristian");
+console.log(misJugadores[2]);
