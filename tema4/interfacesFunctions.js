eb108b3a0421215b0f02a3984849f821280718c4
josemasf <josemasf@gmail.com>
2020-05-07 00:11:26 +0200

    interfaces y funciones

---
+++ tema4/interfacesFunctions.js
@@ -0,0 +1,6 @@
+var sesion;
+sesion = function (email_, password_) {
+    if (email_.length > 10 && password_.length > 10)
+        return true;
+    return false;
+};
---
+++ tema4/interfacesFunctions.ts
@@ -0,0 +1,12 @@
+interface Login{
+    (email: string, password: string): boolean
+}
+
+let sesion: Login;
+sesion = function(email_: string, password_: string){
+    if(email_.length>10 && password_.length>10)
+        return true;
+    return false;
+}
+
+
