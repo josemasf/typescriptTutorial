e6ed642636509ba6b0875368fc4b03b962df936a
josemasf <josemasf@gmail.com>
2020-05-06 19:56:17 +0200

    cración de variables

---
+++ tema2/variables.js
@@ -0,0 +1,8 @@
+//declarar
+var nombreVal;
+//identificar
+nombreVal = true;
+//declarar e identificar
+var equipo = "Córdoba CF";
+//contstantes
+var PI = 3.1416;
---
+++ tema2/variables.ts
@@ -0,0 +1,12 @@
+//declarar
+let nombreVal;
+
+//identificar
+nombreVal = true;
+
+//declarar e identificar
+let equipo ="Córdoba CF";
+
+//contstantes
+
+const PI = 3.1416
