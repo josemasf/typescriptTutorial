8ca93bc678cb14e6ea196a8647c5efdd64902094
josemasf <josemasf@gmail.com>
2020-05-06 20:12:11 +0200

    variables tipo number

---
+++ tema2/number.js
@@ -0,0 +1,9 @@
+var miEdad = 32;
+var miNota = 7.42;
+var miNota2 = 2.42;
+//operacion arimeticos
+console.log(miNota + miNota2);
+console.log(miNota - miNota2);
+console.log(miNota * miNota2);
+console.log(miNota / miNota2);
+console.log(6 % 3);
---
+++ tema2/number.ts
@@ -0,0 +1,11 @@
+let miEdad: number =  32;
+let miNota: number = 7.42;
+let miNota2: number = 2.42;
+
+//operacion arimeticos
+
+console.log(miNota + miNota2);
+console.log(miNota - miNota2);
+console.log(miNota * miNota2);
+console.log(miNota / miNota2);
+console.log(6%3);
