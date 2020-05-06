f705e80b3e984bba955164569b7fda4ff2cf0190
josemasf <josemasf@gmail.com>
2020-05-06 23:39:18 +0200

    parametros ref

--- tema3/parametrosDefault.ts
+++ tema3/parametrosDefault.ts
@@ -1,6 +1,9 @@
 function asignarPosicion(nombre: string, numero: number, posicion: string = "Portero"): string{
     return `${nombre} jugará con el número ${numero} con la posicion ${posicion}`;
 }
-
+///En TS los paramtros son obligatorios
 console.log(asignarPosicion("Juan", 7, "Ala"));
+
+
+///La posición al tener valor defecto, no es necesaario
 console.log(asignarPosicion("Lucas", 1));
