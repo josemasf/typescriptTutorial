f39c28507a50e620109d308f8e21ecd678bd29a5
josemasf <josemasf@gmail.com>
2020-05-07 12:23:33 +0200

    clase con interfaz

---
+++ tema5/interfacesClase.js
@@ -0,0 +1,9 @@
+var Estudiante = (function () {
+    function Estudiante() {
+    }
+    return Estudiante;
+})();
+var estudiante = new Estudiante();
+estudiante.nombre = "Juan";
+estudiante.apellido = "Perez";
+console.log(estudiante);
---
+++ tema5/interfacesClase.ts
@@ -0,0 +1,18 @@
+interface DatosPersonales{
+    nombre: string;
+    apellido: string:
+}
+
+
+class Estudiante implements DatosPersonales{
+    nombre: string;
+    apellido: string;
+}
+
+
+let estudiante = new Estudiante();
+
+estudiante.nombre = "Juan";
+estudiante.apellido="Perez";
+
+console.log(estudiante);
